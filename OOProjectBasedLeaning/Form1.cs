1498d8bae92800fe3ce95592e642505a979b06f9
桃花 坂口 <C3122@oic.jp>
2025-06-19 14:43:26 +0900

    a

---
+++ OOProjectBasedLeaning/Form1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace OOProjectBasedLeaning
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            new Yoyaku().Show();
+
+            new HotelForm().Show();
+
+            new HomeForm().Show();
+
+            new GuestCreatorForm().Show();
+
+        }
+    }
+}
